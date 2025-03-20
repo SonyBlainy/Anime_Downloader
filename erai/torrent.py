fac1370cafcdd0a3279310dba1ae663a0aa4d90d
SonyBlainy <renangustavo.santos@hotmail.com>
2025-03-19 23:41:23 -0300

    Qbit rework

--- erai/torrent.py
+++ erai/torrent.py
@@ -1,29 +1,34 @@
 import os
 import requests
 
-save = os.getenv('save')
-path = os.getenv('caminho')
-api_url = 'http://127.0.0.1:8080/api/v2/'
+class Qbit():
+    def __init__(self):
+        self.api_url = 'http://127.0.0.1:8080/api/v2/'
+        self.sessao = self.init_sessao()
 
-def login() -> requests.Session:
-    if 'qbittorrent.exe' not in os.popen('tasklist').read().lower():
-        os.system('powershell -Command \"qbittorrent.exe\"')
-    sessao = requests.Session()
-    para = {'username': 'admin', 'password': 'admin123'}
-    header = {'Referer': 'http://127.0.0.1:8080'}
-    l = sessao.post(api_url+'auth/login', data=para, headers=header)
-    if l.status_code != 200:
-        return None
-    else:
+    def init_sessao(self):
+        if 'qbittorrent.exe' not in os.popen('tasklist').read().lower():
+            os.system('powershell -Command \"qbittorrent.exe\"')
+        sessao = requests.Session()
+        para = {'username': 'admin', 'password': 'admin123'}
+        header = {'Referer': 'http://127.0.0.1:8080'}
+        l = sessao.post(self.api_url+'auth/login', data=para, headers=header)
+        if l.status_code != 200:
+            return None
         return sessao
+    
+    def baixar(self, ep):
+        self.sessao.post(self.api_url+'torrents/add', {'urls': ep.link, 'savepath': ep.caminho})
 
-def baixar(anime, qbit: requests.Session):
-    qbit.post(api_url+'torrents/add', {'urls': anime.ep.link, 'savepath': anime.ep.caminho})
-
-def infos(qbit: requests.Session):
-    resposta = qbit.get(api_url+'torrents/info')
-    return resposta.json()
-
-def parar(qbit: requests.Session, hash: str):
-    qbit.post(api_url+'torrents/stop', data={'hashes': hash})
-    qbit.post(api_url+'torrents/delete', data={'hashes': hash, 'deleteFiles': 'false'})
+    def infos(self):
+        return self.sessao.get(self.api_url+'torrents/info').json()
+    
+    def parar(self, hash=None, tudo=False):
+        if tudo:
+            infos = self.infos()
+            for torrent in infos:
+                self.sessao.post(self.api_url+'torrents/stop', data={'hashes': torrent['hash']})
+                self.sessao.post(self.api_url+'torrents/delete', data={'hashes': torrent['hash'], 'deleteFiles': 'false'})
+        else:    
+            self.sessao.post(self.api_url+'torrents/stop', data={'hashes': hash})
+            self.sessao.post(self.api_url+'torrents/delete', data={'hashes': hash, 'deleteFiles': 'false'})

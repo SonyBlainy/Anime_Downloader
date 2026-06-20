7c22afb5b966455f9723d55c74f1a769fd3080f4
SonyBlainy <renangustavo.santos@hotmail.com>
2026-06-20 16:11:14 -0300

    Implementação do TopAnimes

--- topanimes/top_animes.py
+++ topanimes/top_animes.py
@@ -1,89 +1,98 @@
-import requests
-from lxml.html import fromstring
+from httpx import AsyncClient as Client
+from bs4 import BeautifulSoup
 from urllib.parse import unquote
 import re
-import yt_dlp
-import json
+import asyncio
+import os
+import time
 
-def pesquisar(nome: str) -> list:
-    api = 'https://q1n.net/'
-    if len(nome.split()) > 1:
-        nome = '+'.join(nome.split())
-    r = fromstring(requests.get(api, params={'s': nome}).content)
-    animes = r.find_class('result-item')
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'}
+
+async def pesquisar(nome: str) -> list:
+    api = 'https://topanimes.net/'
+    async with Client(headers=header) as navegador:
+        pagina = await navegador.get(api, params={'s': nome})
+        pagina = pagina.content
+    pagina = BeautifulSoup(pagina, 'html.parser')
+    animes = pagina.select_one('.search-page').select('.result-item')
     lista = []
     for a in animes:
-        dados = a.find('article')
-        dados = dados.xpath('div')[1]
-        dados = dados.find('div[1]/a')
-        link = dados.get('href')
+        dados = a.select_one('.title>a')
         nome = dados.text
-        dados = {'nome': nome, 'link': link}
+        link = dados.get('href')
+        dados = {'nome': nome, 'link': link, 'server': 'TopAnimes'}
         lista.append(dados)
     return lista
 
-def episodios(anime):
-    lista = []
-    r = fromstring(requests.get(anime.link).content)
-    eps = r.find_class('episodios')[0].findall('li')
+async def episodios(link: str):
+    async with Client(headers=header) as navegador:
+        pagina = await navegador.get(link)
+        pagina = pagina.content
+    pagina = BeautifulSoup(pagina, 'html.parser')
+    eps = pagina.select('.episodios > li')
+    data = {}
     for ep in eps:
-        link = ep.find_class('episodiotitle')[0].find('a')
-        nome = link.text
-        link = link.get('href')
-        ep = player({'nome': nome, 'link': link})
-        if ep:
-            lista.append(ep)
-    if not lista:
-        return None
-    anime.ep = lista
-    return anime
+        nome = ep.select_one('.episodiotitle > a')
+        link = nome.get('href')
+        nome = re.search(r'Episódio (\d*)', nome.text).group(1)
+        data[nome] = link
+    limite = asyncio.Semaphore(3)
+    data = [player(a, limite) for a in data.items()]
+    data = await asyncio.gather(*data, return_exceptions=True)
+    data = [i for i in data if not isinstance(i, Exception)]
+    return dict(data)
 
-def player(ep):
-    r = fromstring(requests.get(ep['link']).content)
-    pagina = r.find('.//div[@id="dooplay_player_content"]').findall('.div')[1:]
-    pagina = [i.find('div/iframe').get('data-litespeed-src') for i in pagina]
-    pagina2 = [i for i in pagina if 'blogger.com' in i]
-    pagina2.extend([i for i in pagina if 'q1n.net/antivirus2' in i])
-    pagina = [unquote(re.findall(r'\?url=(.*$)', i)[0]) for i in pagina if '?url=' in i]
-    pagina.extend(pagina2)
-    fontes = {'csst.online': lambda link: ruplay(link), 'blogger.com': lambda link: blogger(link),
-              }
-    for fonte in fontes.keys():
-        for p in pagina:
-            if fonte in p:
-                try:
-                    ep['link'], ep['estensao'] = fontes[fonte](p)
-                    return ep
-                except:
-                    continue
-    return None
+async def player(ep, limite):
+    async with limite:
+        async with Client(headers=header) as navegador:
+            pagina = await navegador.get(ep[1])
+            pagina = pagina.content
+        pagina = BeautifulSoup(pagina, 'html.parser')
+        link = pagina.select_one('#playcontainer')
+        link = link.find('iframe', src=re.compile(r'csst.online'))
+        if link:
+            link = link.get('src')
+            if 'topanimes.net' in link:
+                link = unquote(link)
+                link = re.search(r'\?url=(.*?)&poster', link).group(1)
+            async with Client(headers=header) as navegador:
+                pagina = await navegador.get(link, follow_redirects=True)
+                pagina = pagina.content
+            pagina = BeautifulSoup(pagina, 'html.parser')
+            js = pagina.select_one('body>script')
+            js = js.text
+            link = re.findall(r'var player = new Playerjs\(\{([\s\S]*?)\}\);', js)[-1]
+            link = re.search(r'\[1080p\](.*?\.mp4)', link).group(1)
+        else:
+            raise ValueError('Nenhum link válido encontrado')
+        return (ep[0], link)
 
-def ruplay(link):
-    r = fromstring(requests.get(link).content)
-    link = r.find('body/script').xpath('text()')[0]
-    pattern = r'var player = new Playerjs\({[^}]*file:"([^"]+)"'
-    t = re.findall(pattern, link)[-1]
-    link = re.search(r'\[1080p\](.*$)', t).group(1)
-    estensao = re.search(r'/[\d]*(\.[\w]*)/', link).group(1)
-    return link, estensao
-
-def blogger(link):
-    yt_config = {
-        'quiet': True,
-        'no_warnings': True,
-        'get_url': True
-    }
-    with yt_dlp.YoutubeDL(yt_config) as ydl:
-        link = ydl.extract_info(link, download=False)['url']
-    return link, '.mp4'
-
-def qn1(link):
-    r = fromstring(requests.get(link).content)
-    link = r.find('body/script').xpath('text()')[0]
-    link = re.findall(r'sources: \[(.*)\],', link)[0]
-    link = json.loads(link)
-    link = link.get('file')
-    head = {'Referer': 'https://q1n.net/', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-    links = str(requests.get(link, headers=head).content, 'utf-8')
-    links = re.findall(r'(https://.*)\r\n', links)
-    return links, '.mp4'
+async def baixar(ep, progresso):
+    extensao = re.search(r'/\d*(\.\w{3})', ep['link']).group(1)
+    caminho = os.path.join(ep['caminho'], ' - '.join(ep['ep'].split())+extensao)
+    with open(caminho, 'wb') as arquivo:
+        async with Client(headers=header) as navegador:
+            async with navegador.stream('GET', ep['link'], follow_redirects=True) as response:
+                baixado = 0
+                total = int(response.headers['content-length'])
+                tempo_utlimo_calculo = time.perf_counter()
+                baixado_intervalo = 0
+                velocidade_atual = 0
+                async for chunck in response.aiter_raw():
+                    arquivo.write(chunck)
+                    tamanho_chunck = len(chunck)
+                    baixado += tamanho_chunck
+                    baixado_intervalo += tamanho_chunck
+                    tempo_agora = time.perf_counter()
+                    delta_tempo = tempo_agora - tempo_utlimo_calculo
+                    if delta_tempo > 0.5:
+                        velocidade_atual = baixado_intervalo/delta_tempo
+                        tempo_utlimo_calculo = tempo_agora
+                        baixado_intervalo = 0
+                        velocidade_atual /= 1024
+                        if velocidade_atual > 1024:
+                            velocidade_atual /= 1024
+                            velocidade_atual = f'{velocidade_atual:.2f}MB/s'
+                        else:
+                            velocidade_atual = f'{velocidade_atual:.2f}KB/s'
+                    progresso(baixado/total, velocidade_atual)
